cd120750636c527988fd175c37ebc788c2998832
Michał Anioł <32655609+MichalAniol@users.noreply.github.com>
2017-11-07 16:09:45 +0100

    Add files via upload

---
+++ kafelki.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="komanty" tilewidth="30" tileheight="30" tilecount="40" columns="10">
+ <image source="komanty.JPG" width="300" height="120"/>
+</tileset>
